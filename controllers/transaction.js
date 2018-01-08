92efab58759fa96fa7b167adfb475297b9a7e0a5
KraProgrammer <m.kranzer@hotmail.com>
2018-01-08 22:52:14 +0100

    api: add transaction first version

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -3,11 +3,11 @@ const db = require('../db');
 /**
  * @title Transaction Route
  *
- * @desc Used get all transaction of journey
+ * @desc Used get all transaction of a journey
  *
  * @method GET
  *
- * @url /user/signup
+ * @url /journey/:journeyId/transaction
  * @data (Header) Bearer token *
  * 
  * @success-code 200
@@ -29,10 +29,40 @@ const db = require('../db');
  * 
  */
 exports.transactionGetAll = (req, res, next) => {
-    res.status(200);
-    res.json({
-        message: 'Success Endpoint test'
-    })
+    
+    const statement = "SELECT journeyId, transactionID, timestamp, currencyId, categoryId, description, array_agg('{' || userId || ',' || amount || '}')" +
+                        'FROM public.transaction JOIN public.userInTransaction USING (journeyid, transactionid) WHERE journeyid IN (' + 
+                            'SELECT journeyid FROM public.journey JOIN userinjourney USING(journeyid) WHERE journeyid = $1 AND userid=$2);';
+
+    const values = [id, req.userData.userId];
+
+    // db.any(statement, values)
+    // .then((transactions) => {
+    //     const statement = "SELECT userId, amount FROM public.userInTransaction WHERE journeyid = $1, transactionid = $2"
+    //     var response = {
+    //         count: transactions.length, 
+    //         transactions: transactions.map(tranaction => {
+    //             const values = [tranaction.journeyId, transaction.transactionID];
+                
+    //             return {
+    //                 transactionID: transaction.transactionID, 
+    //                 timestamp: transaction.timestamp, 
+    //                 currencyId: transaction.currencyId, 
+    //                 categoryId: transaction.categoryId, 
+    //                 description: transaction.description,
+    //                 data: {
+
+    //                 }
+    //             }
+    //         })
+    //     }
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json({
+    //         error: err.message
+    //     });
+    // });
 }
 
 exports.transactionCreate = (req, res, next) => {
