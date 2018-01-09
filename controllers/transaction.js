ebe1eaeab0be48e52b116f6076d88dc33cda5e21
KraProgrammer <m.kranzer@hotmail.com>
2018-01-09 23:13:48 +0100

    api: add firstversion of transaction create

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -18,11 +18,29 @@ const journeySelect = "SELECT journeyid FROM public.journey JOIN public.userInJO
  * @success-code 200
  * @success-content
  * {
- *   "count": 5,
- *        "transactions": [
- *            {
- * 
- *            }
+ *     "count": 1,
+ *     "transactions": [
+ *         {
+ *             "transactionID": 1,
+ *             "journeyId": 5,
+ *             "timestamp": "2018-01-09T19:14:33.401Z",
+ *             "currencyId": 1,
+ *             "categoryId": 1,
+ *             "description": "desc1",
+ *             "data": [
+ *                 {
+ *                     "userid": 1,
+ *                     "amount": 10
+ *                 },
+ *                 {
+ *                     "userid": 2,
+ *                     "amount": 20
+ *                 }
+ *             ],
+ *             "tsum": 30
+ *         }
+ *     ],
+ *     "sum": 30
  * }
  *
  * @error-code 500
@@ -36,10 +54,10 @@ const journeySelect = "SELECT journeyid FROM public.journey JOIN public.userInJO
 exports.transactionGetAll = (req, res, next) => {
    
     const statement = "SELECT transactionid, journeyid, timestamp, description, currencyid, categoryid, array_agg(" + '\'{"userid": \' || userId || \', "amount": \' || amount::money::numeric::float8 || \'}\''+ ") AS data " +
-	"FROM public.transaction " +
-    "JOIN public.userInTransaction USING (journeyid, transactionid) " +
-    "WHERE journeyid IN ( "+ journeySelect + " ) " +
-    "GROUP BY transactionid, journeyId; ";
+                        "FROM public.transaction " +
+                        "JOIN public.userInTransaction USING (journeyid, transactionid) " +
+                       "WHERE journeyid IN ( "+ journeySelect + " ) " +
+                    "GROUP BY transactionid, journeyId; ";
 
     const values = [req.journeyData.id, req.userData.userId];
 
@@ -82,28 +100,144 @@ exports.transactionGetAll = (req, res, next) => {
 
 }
 
+
+/**
+ * @title New Transaction Route
+ *
+ * @desc Used to create a new transaction
+ * 
+ * @method POST
+ *
+ * @url /journey/:journeyId/transaction
+ * @data description
+ * @data currencyid
+ * @data categoryid
+ * @data partition = [{"userid": number, "amount": number}]
+ * @data (Header) Bearer token
+ * 
+ * @success-code 201
+ * @success-content
+ * {
+ *   message: 'Successfully created transaction',
+ *   createdTransaction: resultObject,
+ *   request: {
+ *       type: 'GET', 
+ *       url: http://localhost:3000//journey/:journeyId/transaction
+ * }   
+ *
+ * @error-code 500
+ * @error-content
+ * {
+ *   message: 'Error Message'
+ * }
+ * 
+ * 
+ */
+
 exports.transactionCreate = (req, res, next) => {
-    "INSERT INTO public.transaction("
-        'journeyid, "timestamp", description, currencyid, categoryid)'
-        "VALUES (5, now(), 'desc1', 1, 1);"
+    const statement = "INSERT INTO public.transaction(" +
+    'journeyid, "timestamp", description, currencyid, categoryid)' +
+    "VALUES ($1, now(), $3, $4, $5);" +
+    " RETURNING transactionid, journeyid, timestamp, description, currencyid, categoryid;";
 
-        "INSERT INTO public.userintransaction("
-            "journeyid, transactionid, userid, amount)"
-            "VALUES (5, 1, 1, 10);"
+    const values = [req.journeyData.id, req.userData.userId, req.body.description, req.body.currencyid, req.body.categoryid];
+    const partition = req.body.partition;
 
-    res.status(200);
-    res.json({
-        message: 'Success Endpoint test'
+
+
+    // i am not sure if this is good design
+    if (!(req.body.description && req.body.currencyid && req.body.categoryid)) {
+        return res.status(422).json({
+            message: 'Missing argument'
+        });
+    }
+
+    db.one(statement, values)
+    .then((result) => {
+        const statement = "INSERT INTO public.userintransaction(" +
+                                "journeyid, transactionid, userid, amount)" +
+                                "VALUES ($1, $2, $3, $4);"
+
+        var valuesForInsert = []; /////////////////////////// create multiple insert 
+        foreach (entry in partition) { ////// foreach wrong?
+            valuesForInsert.push([req.journeyData.id, result.transactionid, entry.userid, entry.amout]);
+        }
+
+        db.none(statement, values)
+        .then(() => {
+            res.status(201).json({
+                message: 'Successfully created journey',
+                createdJourney: result,
+                request: {
+                    type: 'GET', 
+                    url: req.protocol + '://' + req.get('host') + req.originalUrl + '/' + result.journeyid
+                }            
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err.message
+            });
+        });
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err.message
+        });
+    });
 }
+
+
+/**
+ * @title Transaction Detail Route 
+ *
+ * @desc Used get all details to a transaction of a journey
+ *
+ * @method GET
+ *
+ * @url /journey/:journeyId/transaction/:transactionid
+ * @data (Header) Bearer token *
+ * 
+ * @success-code 200
+ * @success-content
+ * {
+ *    "transactionID": 1,
+ *    "journeyId": 5,
+ *    "timestamp": "2018-01-09T19:14:33.401Z",
+ *    "currencyId": 1,
+ *    "categoryId": 1,
+ *    "description": "desc1",
+ *    "data": [
+ *        {
+ *            "userid": 1,
+ *            "amount": 10
+ *        },
+ *        {
+ *            "userid": 2,
+ *            "amount": 20
+ *        }
+ *    ],
+ *    "tsum": 30
+ * }
+ *
+ *
+ * @error-code 500
+ * @error-content
+ * {
+ *   message: 'Error Message'
+ * }
+ * 
+ */
 
 exports.transactionGetDetail = (req, res, next) => {
     const transactionId = req.params.transactionId;
     const statement = "SELECT transactionid, journeyid, timestamp, description, currencyid, categoryid, array_agg(" + '\'{"userid": \' || userId || \', "amount": \' || amount::money::numeric::float8 || \'}\''+ ") AS data " +
-	"FROM public.transaction " +
-    "JOIN public.userInTransaction USING (journeyid, transactionid) " +
-    "WHERE journeyid IN ( "+ journeySelect + " ) AND transactionid = $3" +
-    "GROUP BY transactionid, journeyId; ";
+                        "FROM public.transaction " +
+                        "JOIN public.userInTransaction USING (journeyid, transactionid) " +
+                       "WHERE journeyid IN ( "+ journeySelect + " ) AND transactionid = $3" +
+                    "GROUP BY transactionid, journeyId; ";
 
     const values = [req.journeyData.id, req.userData.userId, transactionId];
 
